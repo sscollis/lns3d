c1392ae77b79421743b16c3783b9c38e6fa87325
Scott Collis <sscollis@gmail.com>
2020-02-23 08:33:28 -0700

    Improvements

--- test/vortex/run.sh
+++ test/vortex/run.sh
@@ -8,6 +8,6 @@ echo LNS3D base directory = $LNS3D_DIR
 $LNS3D_DIR/util/genmesh < mesh.inp && \
 ln -f -s grid.dat grid.xyz && \
 $LNS3D_DIR/util/mkvortex < vort.inp && \
-$LNS3D_DIR/src/lns3d < lns3d.inp && \
+$LNS3D_DIR/src/lns3d < lns3d.inp | tee lns3d.log && \
 ./mpost output.R.*
 exit 0
