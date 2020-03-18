6d1b0327c288f2160b5045b07ceb8022b22e3433
Scott Collis <sscollis@gmail.com>
2020-03-18 07:14:21 -0600

    Added datafile headers

--- util/npost.f90
+++ util/npost.f90
@@ -670,6 +670,7 @@
         allocate ( knot(ns+kord), bs(ns,ndof), ynn(ny), uss(ny), unn(ny) )
         open(11,file='loc.dat')
         write(11,"('# ',a)") "i, s(i), x(i,1)"
+        write(39,"('# ',a)") "x(i,1), edge, rhoe, ue, ve, we, te"
         do i = imin, imax, inc
           if (i.gt.0 .and. i.le.nx) then
           call makename(base,i,fname)
