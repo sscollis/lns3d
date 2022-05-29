862c193c57fea30794fea941fe7e1a2a6aed1e0b
S. Scott Collis <sscollis@me.com>
2022-05-29 14:19:34 -0600

    still adjusting path

--- test/wave/run.sh
+++ test/wave/run.sh
@@ -6,13 +6,13 @@ INP="${1:-rk4.inp}"
 LNS3D_DIR="${LNS3D_DIR:=../..}"
 echo LNS3D base directory = $LNS3D_DIR
 echo "  Using input file $INP"
-export PATH=$LNS3D_DIR/util:$PATH
+#export PATH=$LNS3D_DIR/util:$PATH
 echo $PATH
 which verlist
 $LNS3D_DIR/util/genmesh < mesh.inp && \
 ln -f -s grid.dat grid.xyz && \
 $LNS3D_DIR/util/mkmean < mean.inp && \
 $LNS3D_DIR/util/mkdist < dist.inp && \
-env PATH=$LNS3D_DIR/util:$PATH $LNS3D_DIR/src/lns3d < $INP | tee lns3d.log && \
+$LNS3D_DIR/src/lns3d < $INP | tee lns3d.log && \
 ./mpost output.R.*
 exit
