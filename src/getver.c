73a1038ef74119deedd1d6957e24d12ad8e2676a
S. Scott Collis <sscollis@me.com>
2022-05-29 15:39:01 -0600

    Removed warning

--- src/getver.c
+++ src/getver.c
@@ -53,8 +53,8 @@ int igetver_( char *base, char *suf )
   //printf("Calling command = %s\n",command);
   istr = popen(command,"r");
   ptr = fgets(line,256,istr);
-  if (ptr == NULL) 
-    printf("ERROR: NULL returned from fgets() in igetver\n");
+  //if (ptr == NULL) 
+  //  printf("ERROR: NULL returned from fgets() in igetver\n");
   pclose(istr);
   sscanf(line,"%d",&iver);
   if ( strlen(line) < 1 ) iver = 0;
