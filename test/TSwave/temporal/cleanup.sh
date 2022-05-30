a7ee1bf0a6a41d9eec23283e253b01716a558a39
S. Scott Collis <sscollis@me.com>
2022-05-30 10:10:02 -0600

    Fixed

--- test/TSwave/temporal/cleanup.sh
+++ test/TSwave/temporal/cleanup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 \rm output.[Rqh].*
 \rm fort.*
-\rm grid.xyz mag.dat
+\rm grid.xyz mag.dat body.dat cfirst.dat coord.dat cprofile.dat csecond.dat \
+echo.dat efunction.dat first.0 grid.dat inflow.dat mean.dat metric.dat \
+profile.0 second.0 sprofile.dat thick.dat
 exit 0
