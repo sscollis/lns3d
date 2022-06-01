ea1692e45eee97d7b810942d9f6493bfd83c8430
S. Scott Collis <sscollis@me.com>
2022-05-31 19:41:50 -0600

    Added

---
+++ test/CFwave/temporal/README.md
@@ -0,0 +1,18 @@
+# Temporal Tollmein-Schlichting Wave
+
+<p align=center>
+<img src=https://github.com/sscollis/lns3d/blob/master/test/TSwave/temporal/v.png>
+<br>Compressible temporally growing Tollmein-Schlichting wave, contours 
+of vertical velocity.</p>
+
+```bash
+Re =  4.000000E+02, Ma =  3.000000E-01, Pr =  1.000000E+00
+Omega = ( 6.3418480187508E-007, 6.5335847258858E-003)
+Alpha = (-2.8743645100000E-001, 0.0000000000000E+000)
+Beta  = ( 3.5000000000000E-001, 0.0000000000000E+000)
+```
+
+$L_z = 17.951958020513104$
+
+S. Scott Collis\
+sscollis@gmail.com 
---
+++ test/CFwave/temporal/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+LNS3D_DIR=../../..
+FSC_DIR=../../../../fsc
+#
+# Make the mean profile
+#
+$FSC_DIR/fsc < fsc.inp
+tail -n +2 cprofile.dat > profile.0
+tail -n +2 cfirst.dat > first.0
+tail -n +2 csecond.dat > second.0
+#
+# Make the mesh
+#
+$LNS3D_DIR/util/genmesh < genmesh.inp
+\ln -fs grid.dat grid.xyz
+#
+# Make mean flow from FSC profile
+#
+$LNS3D_DIR/util/mkmean < mkmean.inp
+#
+# Make initial condition from time.1 eigenfunction
+#
+$LNS3D_DIR/util/mkeig3d < mkeig3d.inp
+#
+# Run the simulation
+#
+$LNS3D_DIR/src/lns3d < lns3d.inp
+#
+# Postprocess
+#
+./mpost.sh output.R.*
+#
+exit 0
