ae5b48ccb114c3854a553555f6e22db57b82d3f6
S. Scott Collis <sscollis@gmail.com>
2022-06-11 08:55:46 -0600

    Cleanup

--- test/receptivity/wave/run.sh
+++ test/receptivity/wave/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 LNS3D=$HOME/git/lns3d
 $LNS3D/util/genmesh < genmesh.inp && \
+\ln -f -s grid.dat grid.xyz && \
 $LNS3D/util/mkmean < mkmean.inp   && \
 $LNS3D/util/mkdist3d < mkdist3d.inp && \
 $LNS3D/src/lns3d < lns3d.inp && \
