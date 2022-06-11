77881b15ad24fd2248ba62359e36004d452bdddf
S. Scott Collis <sscollis@gmail.com>
2022-06-11 08:38:57 -0600

    Added wave sub-run

---
+++ test/receptivity/wave/lpost.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Plots conservative variables (-c) without Plot3D normalization (-np)
+#
+LNS3D_DIR=${LNS3D_DIR:=../../..}
+for file in $@
+do
+  echo Processing $file
+  $LNS3D_DIR/util/lpost3d -ij -f $file >> lpost.log 
+done
+exit
