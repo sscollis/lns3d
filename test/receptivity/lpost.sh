c062b20ff6798e9ab647b61bcbc400c9ebd29789
S. Scott Collis <sscollis@gmail.com>
2022-06-11 17:11:51 -0600

    fixed paths

--- test/receptivity/lpost.sh
+++ test/receptivity/lpost.sh
@@ -2,7 +2,7 @@
 #
 # Plots conservative variables (-c) without Plot3D normalization (-np)
 #
-LNS3D_DIR=${LNS3D_DIR:=../..}
+LNS3D_DIR=${LNS3D_DIR:=$HOME/git/lns3d}
 for file in $@
 do
   echo Processing $file
