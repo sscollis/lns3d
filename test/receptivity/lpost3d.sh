5ac3c7f01795a9ebff5028f472925d83e5bc7684
S. Scott Collis <sscollis@gmail.com>
2022-06-11 16:58:36 -0600

    Update

--- test/receptivity/lpost3d.sh
+++ test/receptivity/lpost3d.sh
@@ -2,10 +2,10 @@
 #
 # Plots conservative variables (-c) without Plot3D normalization (-np)
 #
-LNS3D_DIR=${LNS3D_DIR:=../..}
+LNS3D_DIR=${LNS3D_DIR:=$HOME/git/lns3d}
 for file in $@
 do
-  echo Processing $file
-  $LNS3D_DIR/util/lpost3d -ij -i $file >> lpost.log 
+  echo Processing file $file
+  $LNS3D_DIR/util/lpost3d -i -ij $file >> lpost3d.log 
 done
 exit
