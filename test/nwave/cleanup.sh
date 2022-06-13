dcc06e38a5583bd1fc8d0157626b96266db27fe2
S. Scott Collis <sscollis@gmail.com>
2022-06-13 15:22:39 -0600

    Fixed nwave run

--- test/nwave/cleanup.sh
+++ test/nwave/cleanup.sh
@@ -1,5 +1,9 @@
-/bin/rm output.*
-/bin/rm *.plt
-cp ic_prim.dat output.R.0
-cp ic_prim.dat output.r.000000
-cp ic_prim.dat output.res
+#!/bin/bash
+\rm -f grid.xyz
+\rm output.*
+\rm *.plt
+\cp ic_prim.dat output.R.0
+\cp ic_prim.dat output.r.000000
+\cp ic_prim.dat output.res
+\rm *.log
+exit 0
---
+++ test/nwave/run_lns3d.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Uses an old SGI generated mesh and metrics to have to swap Endian
+#
+\ln -fs grid.dat grid.xyz
+export GFORTRAN_CONVERT_UNIT='swap'
+cp ic_lns3d.dat output.R.0
+../../src/lns3d < lns3d.inp | tee lns3d.log
+./mpost.sh output.R.* > mpost.log
+exit 0
