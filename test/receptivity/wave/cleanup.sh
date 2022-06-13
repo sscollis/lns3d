d1c320e7ea255089a2e28d76027b27e360d538e5
S. Scott Collis <sscollis@gmail.com>
2022-06-13 12:14:07 -0600

    Removed body.dat

--- test/receptivity/wave/cleanup.sh
+++ test/receptivity/wave/cleanup.sh
@@ -2,5 +2,5 @@
 \rm -f output.[Rqhd].* && \
 \rm -f grid.xyz && \
 \rm -f amp.dat coord.dat echo.dat fort.* *.log mag.dat sponge*.dat
-\rm -f grid.dat ic.dat mean.dat metric.dat
+\rm -f grid.dat ic.dat mean.dat metric.dat body.dat
 exit $?
