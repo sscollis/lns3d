fbfc665d0370c1f3307e335bc77a7e096109a6df
S. Scott Collis <sscollis@gmail.com>
2022-06-13 17:05:08 -0600

    update

---
+++ test/nwave/run_cns2d_prim.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Uses an old SGI generated mesh and metrics to have to swap Endian
+#
+\ln -fs grid.dat grid.xyz
+export GFORTRAN_CONVERT_UNIT='swap'
+cp ic_prim.dat output.r.000000
+../../../cns2d/src/cns2d cns2d_prim.inp | tee cns2d_prim.log
+#./mpost.sh output.R.*
+exit 0
