662a413ccda23c162944188ce33500847b13ec11
S. Scott Collis <sscollis@me.com>
2022-06-21 15:09:22 -0600

    Working on

--- test/pcyl/mean/run.sh
+++ test/pcyl/mean/run.sh
@@ -8,9 +8,13 @@
 #
 # Interpolate from potential solution to new BL mesh
 #
-~/git/lns3d/mesh/interpc -x1 < interpc.inp
+~/git/lns3d/mesh/interpc -x1 -p < interpc.inp
+~/git/lns3d/util/npost output.R.0 
+\mv wall.dat wall.pot
+\mv output.q.0 pot.q
 #
 # Now run lns3d to get BL mean flow
 #
-
+~/git/lns3d/src/lns3d mean.nml < mean.inp
+#
 exit 0
