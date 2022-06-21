3946c1e182704ce8ebb75d91d83c9d946a15c408
S. Scott Collis <sscollis@me.com>
2022-06-21 08:50:31 -0600

    Initial case

--- test/pcyl/run.sh
+++ test/pcyl/run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-echo "Running case $1"
-~/git/lns3d/mesh/confpc -x1 < confpc.inp.$1
+if [[ $# -lt 1 ]]; then
+  echo "Usage:  run.sh case args" 
+  exit 2
+else
+  case=$1
+  shift
+fi
+echo "Running case $case with args $@"
+set -x
+~/git/lns3d/mesh/confpc -x1 $@ < confpc.inp."$case"
 \ln -f -s grid.dat grid.xyz
 ~/git/npot/src/npot < npot.inp.0
 ~/git/npot/src/npot < npot.inp.1
@@ -10,5 +18,5 @@ echo "Running case $1"
 ~/git/npot/src/npot < npot.inp.2
 ~/git/npot/src/npot < npot.inp.1
 ~/git/lns3d/util/npost lns.dat
-cp wall.dat wall.dat.$1
+cp wall.dat wall.dat."$case"
 exit 0
