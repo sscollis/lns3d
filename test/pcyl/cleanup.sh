612987d593b0bd95a059dfbf6513cc6da32e49b8
S. Scott Collis <sscollis@me.com>
2022-06-21 09:03:51 -0600

    Update

--- test/pcyl/cleanup.sh
+++ test/pcyl/cleanup.sh
@@ -1,1 +1,4 @@
-\rm output.* lns.dat wall.dat *.pot grid.dat grid.xyz
+#!/bin/bash
+\rm -f output.* lns.dat wall.dat *.pot grid.dat grid.xyz
+\rm -f body.dat comp.xyz cp.dat lns.dat.q metric.dat restart.dat wall.dat.?
+exit 0
