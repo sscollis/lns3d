23697302c328967c872ac8be64fd32a48e76bcba
S. Scott Collis <sscollis@gmail.com>
2022-07-08 11:20:44 -0600

    Added reference

--- test/pcyl/sweep=35/M=0.8/Re=1e5/lst/cleanup.sh
+++ test/pcyl/sweep=35/M=0.8/Re=1e5/lst/cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 \rm *.dat *.pot field.mean grid.xyz *.log output.[Rqh].* 
-\rm profile.* first.* second.* fort.* eig.* stab.* space.*
+\rm profile.* first.* second.* fort.* eig.* stab.out space.*
 exit $?
