e60110797785eb67597c7ae91d16f15936bdad00
S. Scott Collis <sscollis@gmail.com>
2022-07-15 07:43:52 -0600

    added

---
+++ test/pcyl/sweep=35/M=0.8/Re=1e5/lns/run.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# setup paths
+#
+LNS3D_DIR=${LNS3D_DIR:=$HOME/git/lns3d}
+STAB_DIR=${STAB_DIR:=$HOME/git/stab}
+#
+# Make body-fitted mesh on parabolic cylinder
+#
+$LNS3D_DIR/mesh/pc -y3 < pc.inp
+\ln -f -s grid.dat grid.xyz
+#
+# SSC: Use pre.exe to compute the metrics as they seem to be 
+#      incorrect coming out of pc
+#
+$LNS3D_DIR/pre/src/pre.exe <<EOF
+0
+
+
+1
+EOF
+\mv metric.new metric.dat
+$LNS3D_DIR/mesh/interpc -x3 -y3 -o -p -ij <<EOF
+../ref/grid.dat
+../ref/mean.R.0
+
+0.01 7 .25
+0.001 7 .75
+
+EOF
+\mv output.R.0 mean.R.0
+$LNS3D_DIR/util/npost -p -t -Wc mean.R.0 <<EOF
+1 384 32 
+EOF
+#
+# Run stability analysis (sweep in beta)
+#
+$STAB_DIR/stab < stab.inp
+# Output eigenvector at inflow 
+#
+$STAB_DIR/getevec <<EOF
+evec.dat
+506
+0
+EOF
+\cp space.1 evec.pro 
+exit 0
