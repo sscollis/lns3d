fce92b71e55573cfc91c5ef717f318fe62c36758
S. Scott Collis <sscollis@gmail.com>
2022-07-15 07:43:00 -0600

    removed amp.dat

--- test/CFwave/temporal/cleanup.sh
+++ test/CFwave/temporal/cleanup.sh
@@ -3,5 +3,5 @@
 \rm fort.*
 \rm grid.xyz mag.dat body.dat cfirst.dat coord.dat cprofile.dat csecond.dat \
 echo.dat efunction.dat first.0 grid.dat inflow.dat mean.dat metric.dat \
-profile.0 second.0 sprofile.dat thick.dat
+profile.0 second.0 sprofile.dat thick.dat amp.dat
 exit 0
