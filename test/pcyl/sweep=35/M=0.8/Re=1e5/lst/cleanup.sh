7c090b33353cba4f7c2234c5f275c04f3ccb1565
S. Scott Collis <sscollis@me.com>
2022-08-02 17:01:59 -0600

    Cleanup

--- test/pcyl/sweep=35/M=0.8/Re=1e5/lst/cleanup.sh
+++ test/pcyl/sweep=35/M=0.8/Re=1e5/lst/cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 \rm *.dat *.pot field.mean grid.xyz *.log output.[Rqh].* 
 \rm profile.* first.* second.* fort.* eig.* stab.out space.*
-\rm space-beta.14 sigma-beta.out sigma-s.out
+\rm space-beta.14 sigma-beta.out sigma-s.out efun.* adj.*
 exit $?
