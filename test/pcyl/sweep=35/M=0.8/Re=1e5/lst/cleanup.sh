6115cc499bf0b81ea5ea3a4d9ab23afc361378df
S. Scott Collis <sscollis@me.com>
2022-08-03 08:44:11 -0600

    Remove derived files

--- test/pcyl/sweep=35/M=0.8/Re=1e5/lst/cleanup.sh
+++ test/pcyl/sweep=35/M=0.8/Re=1e5/lst/cleanup.sh
@@ -2,4 +2,5 @@
 \rm *.dat *.pot field.mean grid.xyz *.log output.[Rqh].* 
 \rm profile.* first.* second.* fort.* eig.* stab.out space.*
 \rm space-beta.14 sigma-beta.out sigma-s.out efun.* adj.*
+\rm rho.out t.out u.out w.out stab-s.out stab-beta.out
 exit $?
