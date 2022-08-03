4dd10f0c6959cd253f69d3a1fdb358cbc47f84b4
S. Scott Collis <sscollis@me.com>
2022-08-03 08:21:45 -0600

    Added comments and copyright

--- build.sh
+++ build.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
+#==============================================================================
+#
+# LNS3d simple build script
+#
+# S. Scott Collis
+# Copyright (c)08/03/2022
+#
+#==============================================================================
+#
+# LNS3d
+#
 cd src && \
 \ln -fs gcc.mak Makefile && \
-make clean && make && \
+make clean && make $@ && \
+#
+# LNS3d utilities
+#
 cd ../util && \
 \ln -fs gcc.mak Makefile && \
-make clean && make USE_NR=1 && \
+make clean && make USE_NR=1 $@ && \
+#
+# LNS3d meshers
+#
 cd ../mesh && \
 \ln -fs gcc.mak Makefile && \
-make clean && make USE_NR=1 && \
+make clean && make USE_NR=1 $@ && \
+#
+# LNS3d pre-processors
+#
 cd ../pre/src && \
-\ln -fs gcc.mak Makefile && \
+\ln -fs gcc.mak Makefile $@ && \
 make clean && make
+#
 exit $? 
