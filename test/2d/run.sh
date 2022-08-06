127e124ca72265c7d8e411a805c3ab796e8e54cc
S. Scott Collis <flow.physics.simulation@gmail.com>
2022-08-06 05:42:01 -0600

    Added new test

--- test/2d/run.sh
+++ test/2d/run.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 #
-# This uses input from SGI which requires byte swapping
+# This uses data pre-computed on SGI which requires byte swapping for Intel
 #
+INP="${1:-lns3d.inp}"
+LNS3D_DIR="${LNS3D_DIR:=../..}"
+echo LNS3D base directory = $LNS3D_DIR
+echo "  Using input file $INP"
+#export PATH=$LNS3D_DIR/util:$PATH
+echo $PATH
 ./cleanup.sh
 \cp -r ic.dat output.R.0
-env GFORTRAN_CONVERT_UNIT='swap' ~/git/lns3d/src/lns3d < lns3d.inp
+env GFORTRAN_CONVERT_UNIT='swap' $LNS3D_DIR/src/lns3d < lns3d.inp
 \ln -sf grid.dat grid.xyz
-env GFORTRAN_CONVERT_UNIT='swap' ../../util/npost output.R.0
-env GFORTRAN_CONVERT_UNIT='swap' ../../util/npost output.R.1
-env GFORTRAN_CONVERT_UNIT='swap' ../../util/npost output.R.2
-env GFORTRAN_CONVERT_UNIT='swap' ../../util/npost output.R.3
+env GFORTRAN_CONVERT_UNIT='swap' $LNS3D_DIR/util/npost output.R.0
+env GFORTRAN_CONVERT_UNIT='swap' $LNS3D_DIR/util/npost output.R.1
+env GFORTRAN_CONVERT_UNIT='swap' $LNS3D_DIR/util/npost output.R.2
+env GFORTRAN_CONVERT_UNIT='swap' $LNS3D_DIR/util/npost output.R.3
 exit 0
