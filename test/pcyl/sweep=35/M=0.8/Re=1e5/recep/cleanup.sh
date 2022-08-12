074105e704bcff919ca9fe667ab216b47964fdce
S. Scott Collis <sscollis@gmail.com>
2022-08-12 11:38:15 -0600

    Added plot output

--- test/pcyl/sweep=35/M=0.8/Re=1e5/recep/cleanup.sh
+++ test/pcyl/sweep=35/M=0.8/Re=1e5/recep/cleanup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -e
 \rm -f output.* lns.dat wall.dat *.pot grid.dat grid.xyz lns.q.* *.log
 \rm -f body.dat comp.xyz cp.dat lns.dat.q metric.dat restart.dat wall.dat.*
 \rm -f bl3d.dat delta.dat delta2.dat echo.dat edge.dat mean.q.0 pot.q stat.dat
 \rm -f betah.dat fort.* omega-d.dat coord.dat field.mean filter.dat loc.dat mean.R.0
 \rm -f pedge.dat top.dat profile.* first.* second.* eig.pro *.dat *.out space.1
-exit 0
+\rm -f dke.tex plot.aux plot.pdf
+exit $? 
